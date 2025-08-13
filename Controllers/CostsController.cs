51275e658da5fa487ecdae30c04a688eac7d2aa2
BryanSouceBoyz <bryan050613@gmail.com>
2025-08-13 03:04:10 -0400

    Add files via upload

---
+++ Controllers/CostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoutesCalculator.Application.Contracts;
+using RoutesCalculator.Domain.Entities;
+
+namespace RoutesCalculator.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CostsController : ControllerBase
+    {
+        private readonly ICostService _costService;
+        public CostsController(ICostService costService) => _costService = costService;
+
+        [HttpPost("calculate")]
+        public IActionResult Calculate([FromBody] TripInput input)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var options = _costService.CalculateAll(input).ToList();
+            var best = options.First();
+            return Ok(new { best, options });
+        }
+    }
+}
